7332a8e66386f09c7a766133dc83566fc563a113
Damjan Georgievski <gdamjan@gmail.com>
2015-08-18 16:25:14 +0200

    more readable bash and template functions for output

--- ssl/check_ssl_expiry.sh
+++ ssl/check_ssl_expiry.sh
@@ -7,23 +7,30 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-ispis=""
-while read domain
+function PANIC () {
+  echo -e "$1 days ago did the ssl cert for $2 expire.\e[41m(PANIC)\e[49m\n"
+}
+function WARN () {
+  echo -e "\e[7m$1\e[27m days until \e[7m$2\e[27m ssl cert expires (on $3). \e[7m(WARNING)\e[27m"
+}
+function INFO () {
+  echo -e "$1 days until $2 ssl cert expires (on $3)."
+}
+
+egrep -v '^#' $1 | egrep -v '^[[:space:]]*$' |
+( while read domain
 do
-    if [[ ! $domain =~ ^# ]]; then
-        cert_expires=`echo | openssl s_client -servername $domain -connect $domain:443 2>/dev/null | openssl x509 -noout -dates| grep notAfter|cut -f2 -d=`
-        today=`date +%s`
-        cert_expires_formated=`date --date="$cert_expires" +%s`
-        expires_days=$((($cert_expires_formated - $today)/60/60/24))
+    cert=`openssl s_client -servername $domain -connect $domain:443 </dev/null 2>/dev/null`
+    cert_expires=`echo "$cert" | openssl x509 -noout -enddate | cut -f2 -d=`
+    today=`date +%s`
+    cert_expires_formated=`date --date="$cert_expires" +%s`
+    expires_days=$((($cert_expires_formated - $today)/60/60/24))
 
-        if [[ "$expires_days" -lt "0" ]]; then
-            ispis+="$expires_days days ago did the ssl cert for $domain expire.\e[41m(PANIC)\e[49m\n"
-        elif [[ "$expires_days" -lt "$WARNDAYS" ]]; then
-            ispis+="\e[7m$expires_days\e[27m days until \e[7m$domain\e[27m ssl cert expires (on $cert_expires). \e[7m(WARNING)\e[27m\n"
-        else
-            ispis+="$expires_days days until $domain ssl cert expires (on $cert_expires).\n"
-        fi
+    if [[ "$expires_days" -lt "0" ]]; then
+        PANIC $expires_days $domain
+    elif [[ "$expires_days" -lt "$WARNDAYS" ]]; then
+        WARN $expires_days $domain $cert_expires
+    else
+        INFO $expires_days $domain $cert_expires
     fi
-
-done < $1
-echo -e $ispis | sort --reverse --general-numeric-sort --ignore-leading-blanks
+done ) | sort --reverse --general-numeric-sort --ignore-leading-blanks
